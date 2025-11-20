a271c937ebb7ef61c33291587bde6c1745cd6aa8
Tech-Nickkk <nikhildhakad712@gmail.com>
2025-11-20 08:46:49 +0530

    Change the cursor while dragging

--- index.js
+++ index.js
@@ -607,7 +607,6 @@ function createQuestion(questionData, creating = false) {
 
   questionEditControlls.addEventListener("mousedown", handleDragStart);
   questionEditControlls.addEventListener("touchstart", handleDragStart, { passive: false });
-  questionEditControlls.style.cursor = "grab";
 
   detailedEditBtn.addEventListener("mousedown", () => {
     setTimeout(() => {
